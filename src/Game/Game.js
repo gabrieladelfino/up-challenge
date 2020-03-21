89412c82ee9c37958c93f96f28fa6e36e4db23f4
gabrieladelfino <gabriela.delfino@outlook.com.br>
2020-03-21 12:38:36 -0300

    Remove elementos não utilizados

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Container } from './Game.style'
-import { Input, Button, Image } from '../components'
 import theme from '../utils/theme'
-import carImage from '../assets/CARRO.png'
 
 const Game = () => (
     <Container>
